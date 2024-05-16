2560f09d508fd784be5a0bbeca9e5a546307ded8
Josiah <joehuergo@gmail.com>
2024-05-16 12:49:16 -0400

    Add localStorage saving and loading

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 var clicks = 0;
 
+function loadData() {
+    if (localStorage.hasOwnProperty("clicks")) {
+        clicks = localStorage.getItem("clicks");
+        updateClicker();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadData();
+});
+
 function clickFuck() {
     clicks++;
+    updateClicker();
+    localStorage.setItem("clicks", clicks);
+}
+
+function updateClicker() {
     document.getElementById("clicker").innerHTML = clicks;
 }
